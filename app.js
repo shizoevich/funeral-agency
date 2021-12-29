e28c4a34b62a68b12b06955c562fad14e32c0bfb
Shizoevich <94996568+Shizoevich@users.noreply.github.com>
2021-12-29 03:27:46 +0200

    adding files

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const config = require('config')
+const path = require('path')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const PORT = config.get('port')||5000
+
+
+app.use(express.json({extended:true}))
+app.use('/api/auth', require('./routes/auth.routes'))
+
+
+
+
+
+async function start() {
+    try {
+      await mongoose.connect(config.get('mongoUri'), {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        
+      })
+      app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+    } catch (e) {
+      console.log('Server Error', e.message)
+      process.exit(1)
+    }
+  }
+  
+  start()
